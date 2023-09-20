a82a98259f33fca54feacb6f943c9a27798ff8fd
Dhruv Mohindru <dmohindru@gmail.com>
2023-09-20 21:37:08 +1000

    completed userland program to test calculator device

--- userland-program/ch06/calculator_ioctl_fun.c
+++ userland-program/ch06/calculator_ioctl_fun.c
@@ -10,17 +10,17 @@ enum operator {
 
 char *operator_string[] ={"Add", "Sub", "Div", "Mul"};
 
-int main(int argc, char **argv) {
-    int fd = open("/dev/calculator", O_RDWR);
-    long first_op = 500, result, second_op;
-    enum operator op;
-    ssize_t num_bytes, bytes_written;
-    
-    if (fd == -1) {
-        perror("open");
+int reset_device(int fd) {
+    if (ioctl(fd, CALCULATOR_RESET) < 0) {
+        perror("ioctl");
         return 1;
     }
+    return 0;
+}
 
+int print_current_state(int fd) {
+    long second_op;
+    enum operator op;
     // query for second operator
     if (ioctl(fd, CALCULATOR_QUERY_OPERAND, &second_op) < 0) {
         perror("ioctl");
@@ -35,41 +35,65 @@ int main(int argc, char **argv) {
 
     printf("Current operator [%s], second operand [%ld]\n", operator_string[op], second_op);
 
-    printf("Writing first operand [%ld] to calculator device\n", first_op);
-    write(fd, &first_op, sizeof(long));
+    return 0;
+}
 
-    read(fd, &result, sizeof(long));
-    printf("Updated value of result [%ld]\n", result);
-
-    second_op = 100;
-    if (ioctl(fd, CALCULATOR_SET_OPERAND, &second_op) < 0) {
+int set_second_operand(int fd, long second_operand) {
+    if (ioctl(fd, CALCULATOR_SET_OPERAND, &second_operand) < 0) {
         perror("ioctl");
         return 1;
     }
+    return 0;
+}
 
-    printf("Writing first operand [%ld] to calculator device\n", first_op);
-    write(fd, &first_op, sizeof(long));
-
-    read(fd, &result, sizeof(long));
-    printf("Updated value of result [%ld]\n", result);
-
-    op = SUB;
+int set_operator(int fd, enum operator op) {
     if (ioctl(fd, CALCULATOR_SET_OPERATOR, &op) < 0) {
         perror("ioctl");
         return 1;
     }
+    return 0;
+}
 
+int main(int argc, char **argv) {
+    int fd = open("/dev/calculator", O_RDWR);
+    long first_op = 500, result, second_op;
+    enum operator op;
 
-    // query for current operator
+    
+    if (fd == -1) {
+        perror("open");
+        return 1;
+    }
 
-//    ssize_t num_bytes = read(fd, &result, sizeof(long));
-//    printf("Current value of result [%ld], number of bytes read [%ld]\n", result, num_bytes);
-//
-//    printf("Writing first operand [%ld] to calculator device\n", first_op);
-//    ssize_t bytes_written = write(fd, &first_op, sizeof(long));
-//
-//    num_bytes = read(fd, &result, sizeof(long));
-//    printf("Updated value of result [%ld], number of bytes read [%ld]\n", result, num_bytes);
+    reset_device(fd);
+
+    print_current_state(fd);
+
+    second_op = 100;
+    set_second_operand(fd, second_op);
+
+    print_current_state(fd);
+
+    printf("Writing first operand [%ld] to calculator device\n", first_op);
+    write(fd, &first_op, sizeof(long));
+
+    read(fd, &result, sizeof(long));
+    printf("Updated value of result [%ld]\n", result);
+
+    reset_device(fd);
+
+    // this operation can only be done by admin users
+    set_operator(fd, SUB);
+    second_op = 200;
+    set_second_operand(fd, second_op);
+
+    print_current_state(fd);
+
+    printf("Writing first operand [%ld] to calculator device\n", first_op);
+    write(fd, &first_op, sizeof(long));
+
+    read(fd, &result, sizeof(long));
+    printf("Updated value of result [%ld]\n", result);
 
     return 0;
 }
