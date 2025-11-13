e5dca40696d4030c9ac1298c9b739f6f348679ff
Dhruv Mohindru <dmohindru@gmail.com>
2025-11-13 19:10:38 +1100

    making progress for running tests

---
+++ kmsgpipe/lib/src/kmsgpipe.c
@@ -0,0 +1,11 @@
+#include "kmsgpipe.h"
+
+// int kmsgpipe_init(
+//     kmsgpipe_buffer_t *buf,
+//     uint8_t *base,
+//     kmsg_record_t *records,
+//     size_t capacity,
+//     size_t data_size)
+// {
+//     return 1;
+// }
---
+++ kmsgpipe/lib/tests/test_kmsgpipe.c
@@ -0,0 +1,20 @@
+#include "kmsgpipe.h"
+#include "unity.h"
+
+void setUp(void)
+{
+    // kmsgpipe_init()
+    // kmsgpipe_push()
+    // kmsgpipe_pop
+}
+
+void tearDown(void)
+{
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+
+    return UNITY_END();
+}
